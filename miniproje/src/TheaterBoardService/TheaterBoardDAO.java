5ba2ff97867dd20ec064cdcf28763ab4307ac969
admin <admin@YD02-19>
2021-05-14 17:43:49 +0900

    인생,,

---
+++ miniproje/src/TheaterBoardService/TheaterBoardDAO.java
@@ -0,0 +1,65 @@
+package TheaterBoardService;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Common.DBcon;
+import projectVO.ShowVO;
+
+
+
+public class TheaterBoardDAO {
+	Connection conn;
+	PreparedStatement psmt;
+	ResultSet rs;
+	
+	public List<ShowVO> MiniboardList() {
+		List<ShowVO> list = new ArrayList<ShowVO>();
+		String sql = "select board_id, board_title, user_name, board_date, board_hit";
+		try {
+			conn = DBcon.getConnect();
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				ShowVO vo = new ShowVO();
+				vo.setShow_Name(rs.getNString("Show_name"));
+				vo.setShow_Startday(rs.getNString("Show_startday"));
+				vo.setShow_Endday(rs.getNString("Show_endday"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return list;
+	}
+
+	// Close
+	public void close() {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (psmt != null) {
+			try {
+				psmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ miniproje/src/TheaterBoardService/TheaterBoardVO.java
@@ -0,0 +1,45 @@
+package TheaterBoardService;
+
+public class TheaterBoardVO {
+
+	private int board_id;
+	private String board_title;
+	private String user_name;
+	private String board_date;
+	private int board_hit;
+	
+	
+	public int getBoard_id() {
+		return board_id;
+	}
+	public void setBoard_id(int board_id) {
+		this.board_id = board_id;
+	}
+	public String getBoard_title() {
+		return board_title;
+	}
+	public void setBoard_title(String board_title) {
+		this.board_title = board_title;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getBoard_date() {
+		return board_date;
+	}
+	public void setBoard_date(String board_date) {
+		this.board_date = board_date;
+	}
+	public int getBoard_hit() {
+		return board_hit;
+	}
+	public void setBoard_hit(int board_hit) {
+		this.board_hit = board_hit;
+	}
+	
+	
+	
+}

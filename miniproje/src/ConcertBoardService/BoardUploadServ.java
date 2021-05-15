cdf80cad6c6a0657ec83733ed6da36ccbaeba378
Banya <hello_vanny@naver.com>
2021-05-16 02:48:19 +0900

    Concert part Board work : 반야

--- miniproje/src/ConcertBoardService/BoardUploadServ.java
+++ miniproje/src/ConcertBoardService/BoardUploadServ.java
@@ -21,18 +21,16 @@ public class BoardUploadServ extends HttpServlet {
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setCharacterEncoding("utf-8");
+		req.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html;charset=utf-8");
 		
-		String id = req.getParameter("memberId");
-		String name = req.getParameter("memberName");
+		String name = req.getParameter("name");
 		String title = req.getParameter("title");
 		String contents = req.getParameter("contents");
 		
 		ConcertBoardDAO dao = new ConcertBoardDAO();
 		ConcertBoardVO vo = new ConcertBoardVO();
 		
-		vo.setMemberId(id);
 		vo.setMemberName(name);
 		vo.setTitle(title);
 		vo.setContents(contents);
@@ -40,8 +38,7 @@ public class BoardUploadServ extends HttpServlet {
 		ConcertBoardVO cvo = dao.insertContents(vo);
 		
 		JSONObject obj = new JSONObject();
-		obj.put("memberId", cvo.getMemberId());
-		obj.put("member_name", cvo.getMemberName());
+		obj.put("memberName", cvo.getMemberName());
 		obj.put("title", cvo.getTitle());
 		obj.put("contents", cvo.getContents());
 		

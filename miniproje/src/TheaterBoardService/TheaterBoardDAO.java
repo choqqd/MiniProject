8330fcc9ad48e7f75b785471de959f504ed73146
HotakShin <hotakshin@gmail.com>
2021-05-16 23:37:50 +0900

    호탁 게시판DAO,VO,servlet 작업

--- miniproje/src/TheaterBoardService/TheaterBoardDAO.java
+++ miniproje/src/TheaterBoardService/TheaterBoardDAO.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Common.DBcon;
-import projectVO.ShowVO;
 
 
 
@@ -17,18 +16,20 @@ public class TheaterBoardDAO {
 	PreparedStatement psmt;
 	ResultSet rs;
 	
-	public List<ShowVO> MiniboardList() {
-		List<ShowVO> list = new ArrayList<ShowVO>();
-		String sql = "select board_id, board_title, user_name, board_date, board_hit";
+	public List<TheaterVO> MiniboardList() {
+		List<TheaterVO> list = new ArrayList<TheaterVO>();
+		String sql = "select board_num, board_title, user_name, board_date, board_hit from Theater_Board";
 		try {
 			conn = DBcon.getConnect();
 			psmt = conn.prepareStatement(sql);
 			rs = psmt.executeQuery();
 			while (rs.next()) {
-				ShowVO vo = new ShowVO();
-				vo.setShow_Name(rs.getNString("Show_name"));
-				vo.setShow_Startday(rs.getNString("Show_startday"));
-				vo.setShow_Endday(rs.getNString("Show_endday"));
+				TheaterVO vo = new TheaterVO();
+				vo.setBoard_num(rs.getInt("board_num"));
+				vo.setUser_name(rs.getString("user_name"));
+				vo.setBoard_title(rs.getString("board_title"));
+				vo.setBoard_date(rs.getString("board_date"));
+				vo.setBoard_hit(rs.getInt("board_hit"));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -43,6 +44,43 @@ public class TheaterBoardDAO {
 
 	// 게시글 전체 리스트 뽑아오기
 	
+	
+	public void MiniboardInsert(TheaterVO vo) {
+		
+		String sql = "insert into theater_board(board_num, user_name, board_title, board_hit, board_date) "
+				+ "values ((select max(board_num)+1 from theater_board), ?,?,?,?)";
+				
+		conn = DBcon.getConnect();
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, vo.getUser_name());
+			psmt.setString(2, vo.getBoard_title());
+			psmt.setString(3, vo.getBoard_date());
+			psmt.setInt(4, vo.getBoard_hit());
+			int r = psmt.executeUpdate();
+			System.out.println(r + "건 입력됨");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+
+	}
+	
+	public void MiniboardDelete(TheaterVO vo) {
+		conn = DBcon.getConnect();
+		String sql = "delete from theater_board where board_num = ?";
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, vo.getBoard_num());
+			int r = psmt.executeUpdate();
+			System.out.println(r + " 개의 게시물 제거됌");
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+	}
 	// Close 메소드
 
 	public void close() {
--- miniproje/src/TheaterBoardService/TheaterVO.java
+++ miniproje/src/TheaterBoardService/TheaterVO.java
@@ -2,4 +2,52 @@ package TheaterBoardService;
 
 public class TheaterVO {
 
+	private int board_num;
+	private String board_title;
+	private String user_name;
+	private String board_contents;
+	private String board_date;
+	private int board_hit;
+	
+	
+	
+	
+	public String getBoard_contents() {
+		return board_contents;
+	}
+	public void setBoard_contents(String board_contents) {
+		this.board_contents = board_contents;
+	}
+	public int getBoard_num() {
+		return board_num;
+	}
+	public void setBoard_num(int board_num) {
+		this.board_num = board_num;
+	}
+	public String getBoard_title() {
+		return board_title;
+	}
+	public void setBoard_title(String board_title) {
+		this.board_title = board_title;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getBoard_date() {
+		return board_date;
+	}
+	public void setBoard_date(String board_date) {
+		this.board_date = board_date;
+	}
+	public int getBoard_hit() {
+		return board_hit;
+	}
+	public void setBoard_hit(int board_hit) {
+		this.board_hit = board_hit;
+	}
+	
+	
 }

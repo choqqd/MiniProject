be21bcaa22d4a1bf20260469e828b695ec623a42
Banya <hello_vanny@naver.com>
2021-05-17 13:50:47 +0900

    MiniProject DBCon, WEB.xml Edit : 반야

--- miniproje/src/Common/DBcon.java
+++ miniproje/src/Common/DBcon.java
@@ -1,17 +1,19 @@
 package Common;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 public class DBcon {
 	static Connection conn;
 
 	public static Connection getConnect() {
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@project.cujgaabefr8x.ap-northeast-2.rds.amazonaws.com:1521:orcl", "yedam", "oracle11");
-		} catch (ClassNotFoundException | SQLException e) {
+			InitialContext ic = new InitialContext();
+			javax.sql.DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/mini");
+			conn = ds.getConnection();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return conn;
